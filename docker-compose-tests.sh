ab2836b5241b3ac2d1cd52ed61c1f5b1e17ecd74
Sebastiaan Mannem <sebas@mannem.nl>
2021-11-16 17:53:28 +0100

    defining a docker-compose-tests.sh

--- docker-compose-tests.sh
+++ docker-compose-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -x
 set -e
-docker-compose up -d ldap postgres
+docker-compose up -d --scale postgres=3
+for ((i=1;i<4;i++)); do
+  docker exec -ti pgroute66_postgres_$i /entrypoint.sh background
+done
 
-docker-compose build --no-cache --force-rm
 docker-compose up pgfga || exit 2
 cat testdata/pgtester/tests.yaml | docker-compose run pgtester pgtester || exit 3
 
--- docker/postgres/entrypoint.sh
+++ docker/postgres/entrypoint.sh
@@ -12,12 +12,10 @@ function primary() {
     chown postgres: "${PGDATA}"
   fi
 
-  re="^([0-9.]+)(.*)$"
-  
   if [ ! -e "${PGDATA}/PG_VERSION" ]; then
     PWFILE=$(mktemp)
     echo "${PGPASSWORD}" > "${PWFILE}"
-    initdb --pwfile="${PWFILE}"
+    initdb --pwfile="${PWFILE}" || return $?
     rm "${PWFILE}"
     mkdir "${PGDATA}/conf.d"
     echo "include_dir 'conf.d'" >> "${PGDATA}/postgresql.conf"
@@ -27,26 +25,41 @@ function primary() {
       echo "
 host    all             postgres        ${IP}               md5
 host    replication     postgres        ${IP}               md5" >> "${PGDATA}/pg_hba.conf"
+
+      re="^([0-9.]+)(.*)$"
       if [[ $IP =~ $re ]]; then
         [ -n ${BASH_REMATCH[1]} -a ${BASH_REMATCH[1]} != '127.0.0.1' ] && MYIP=${BASH_REMATCH[1]}
       fi
     done <<< "$(ip a | sed -n '/inet /{s/.*inet //;s/ .*//;p}')"
   fi
-  
-  postgres -D "${PGDATA}"
 }
 
 function standby() {
   PGVERSION=${PGVERSION:-12}
+  PGTARGET_SESSION_ATTRS=read-write
+  export PGHOST=${PGHOSTS}
   export PGDATA=${PGDATA:-/var/lib/pgsql/${PGVERSION}/data}
   if [ ! -e "${PGDATA}" ]; then
     mkdir -p "${PGDATA}"
     chown postgres: "${PGDATA}"
   fi
 
-  [ -e "${PGDATA}/PG_VERSION" ] || pg_basebackup -R -D "${PGDATA}"
-  chmod 0700 "${PGDATA}"
+  if [ ! -e "${PGDATA}/PG_VERSION" ]; then
+    pg_basebackup -R -D "${PGDATA}" || return $?
+    chmod 0700 "${PGDATA}"
+  fi
+}
+
+function pg_start() {
   postgres -D "${PGDATA}"
+}
+
+function pg_start_bg() {
+  pg_ctl start -D "${PGDATA}"
+}
+
+function pg_stop_bg() {
+  pg_ctl start -D "${PGDATA}"
 }
 
 function waitsleep() {
@@ -63,8 +76,13 @@ case "${1}" in
   standby)
     standby
     ;;
+  background)
+    standby || primary
+    pg_start_bg
+    ;;
   auto)
     standby || primary
+    pg_start
     ;;
   sleep)
      waitsleep
